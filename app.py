a6aec2de7b06972926eb1600b409704d6d4742de
ardasumbul1 <arda.sumbul@ozu.edu.tr>
2023-01-29 17:36:16 +0300

    Flask and main page added
    html file for main page is added

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from flask import Flask,render_template
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 def sentiment_score(text):
@@ -11,3 +12,12 @@ def sentiment_score(text):
         return "neutral"
 
 
+
+app = Flask(__name__)
+
+@app.route("/")
+def main_page():
+    return render_template("mainpage.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
